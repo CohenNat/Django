021025b0e706a4985faee45f9cd324a9486c8bc6
CohenNat <147045032+CohenNat@users.noreply.github.com>
2024-06-18 15:57:02 +0200

    ADD: Interface User

--- multilang_site/main/views.py
+++ multilang_site/main/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.utils.translation import gettext as _
 
 def article_list(request):
     articles = Article.objects.all().order_by('-publication_date')
     return render(request, 'main/article_list.html', {'articles': articles})
+
+def article_detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'main/article_detail.html', {'article': article})
 
 def my_view(request):
     output = _("Welcome to my site.")
